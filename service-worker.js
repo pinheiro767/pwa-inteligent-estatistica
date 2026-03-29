7c921c82c4f08cee02c9c8911250b161820d94e6
pinheiro767 <claudiapinheiro767@gmail.com>
2026-03-28 22:00:33 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', e=>{
+  e.waitUntil(
+    caches.open('app').then(cache=>
+      cache.addAll([
+        '/',
+        '/index.html',
+        '/app.js',
+        '/stats.js',
+        '/styles.css'
+      ])
+    )
+  );
+});
