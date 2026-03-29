d4e8ebdb84b17047045c6173a3dac845ccd3ee3c
pinheiro767 <claudiapinheiro767@gmail.com>
2026-03-28 22:42:12 -0300

    Update stats.js

--- stats.js
+++ stats.js
@@ -1,29 +1,20 @@
-function media(arr){
-  return arr.reduce((a,b)=>a+b,0)/arr.length;
+function media(a){
+  return a.reduce((s,v)=>s+v,0)/a.length
 }
 
-function desvio(arr){
-  let m = media(arr);
-  return Math.sqrt(arr.reduce((s,v)=>s+(v-m)**2,0)/arr.length);
+function desvio(a){
+ let m=media(a)
+ return Math.sqrt(a.reduce((s,v)=>s+(v-m)**2,0)/a.length)
 }
 
 function regressao(x,y){
-  let mx = media(x);
-  let my = media(y);
-
-  let num = x.reduce((s,xi,i)=>s+(xi-mx)*(y[i]-my),0);
-  let den = x.reduce((s,xi)=>s+(xi-mx)**2,0);
-
-  let a = num/den;
-  let b = my - a*mx;
-
-  return {a,b};
-}
-
-function prever(x,a,b){
-  return a*x + b;
+ let mx=media(x), my=media(y)
+ let num=x.reduce((s,xi,i)=>s+(xi-mx)*(y[i]-my),0)
+ let den=x.reduce((s,xi)=>s+(xi-mx)**2,0)
+ let a=num/den, b=my-a*mx
+ return {a,b}
 }
 
 function pValue(t){
-  return Math.exp(-0.717*t - 0.416*t*t);
+ return Math.exp(-0.717*t - 0.416*t*t)
 }
